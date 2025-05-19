90356856b80bc5182112860a52c2ac762a176a4b
Fabio Ortiz Charris <89431787+Fajoce@users.noreply.github.com>
2025-05-18 20:57:46 -0500

    Update MateriasController.cs

--- Presentation.InterRapisimo/Controllers/MateriasController.cs
+++ Presentation.InterRapisimo/Controllers/MateriasController.cs
@@ -14,7 +14,7 @@ namespace Presentation.InterRapisimo.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+  
     public class MateriasController : BaseApiController
     {
     
